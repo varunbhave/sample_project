e06fe75d6b17e522e182b711cfd824ea2d153c8f
varunbhave <102780277+varunbhave@users.noreply.github.com>
2022-04-01 09:41:17 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class FirstProgram  {
+
+
+     public static void main(String args[])
+     {
+         System.out.println("hellow world!");
+         Scanner sc= new Scanner(System.in);
+         System.out.println("enter the name");
+         String name=sc.next()
+         System.out.println(name);
+
+
+     }
+}
